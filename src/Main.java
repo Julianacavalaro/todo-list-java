cab903e0f46498c0b0b4d59d57cdbb8058cfc45a
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-10-30 14:15:29 -0300

    Implementa cases do switch

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-     //   Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         boolean executando = true;
+        Tarefa novaTarefa = new Tarefa(); //instância do objeto
         List<Tarefa> listaTarefas = new ArrayList<>();
         List<Tarefa> listaTarefasPendentes = new ArrayList<>();
-        while(executando) {
+        List<Tarefa> listarPorTitulo = new ArrayList<>();
+        List<Tarefa> listaTarefasConcluidas = new ArrayList<>();
+
+        while (executando) {
 
             int opcao = Menu.exibeMenu();
 
             executando = switch (opcao) {
 
                 case 1 -> {
-                    Tarefa novaTarefa = new Tarefa(); //instância do objeto
+
                     var tarefa = novaTarefa.addTarefa();
                     System.out.println(tarefa);
                     listaTarefas.add(tarefa);
@@ -25,30 +29,52 @@ public class Main {
                 }
                 case 2 -> {
                     System.out.println("Listar tarefas pendentes");
-                    for(Tarefa tarefa : listaTarefas){
-                            if(!tarefa.isConcluida()){
-                                listaTarefasPendentes.add(tarefa);
-                            }
+                    for (Tarefa tarefa : listaTarefas) {
+                        if (!tarefa.isConcluida()) {
+                            listaTarefasPendentes.add(tarefa);
+                        }
                     }
                     System.out.println(listaTarefasPendentes);
                     yield true;
                 }
                 case 3 -> {
                     System.out.println("Buscar tarefa por título");
+                    System.out.println("Digite o título para buscar: ");
+                    String titulo = sc.nextLine();
+                    for (Tarefa tarefa : listaTarefas) {
+                        if (tarefa.getTitulo().equals(titulo)) {
+                            listarPorTitulo.add(tarefa);
+                        }
+                    }
+                    System.out.println(listarPorTitulo);
                     yield true;
                 }
                 case 4 -> {
                     System.out.println("Marcar tarefas como concluida");
-
+                    System.out.println("Digite o título para marcar como concluida: ");
+                    String titulo = sc.nextLine();
+                    for (Tarefa tarefa : listaTarefas) {
+                        if (tarefa.getTitulo().equals(titulo)) {
+                            tarefa.setConcluida(true);
+                            listaTarefasPendentes.remove(tarefa);
+                            listaTarefasConcluidas.add(tarefa);
+                        }
+                    }
+                    System.out.println(listaTarefasConcluidas);
                     yield true;
                 }
                 case 5 -> {
                     System.out.println("Remover uma Tarefa");
+                    System.out.println("Digite o título para remover uma tarefa: ");
+                    String titulo = sc.nextLine();
+                    listaTarefas.removeIf(tarefa -> tarefa.getTitulo().equalsIgnoreCase(titulo));
+                    listaTarefasPendentes.removeIf(tarefa -> tarefa.getTitulo().equalsIgnoreCase(titulo));
+                    System.out.println("Todas as tarefas com esse título foram removidas.");
                     yield true;
                 }
                 case 0 -> {
                     System.out.println(listaTarefas);
-                       yield  false;
+                    yield false;
                 }
                 default -> {
                     System.out.println("Opcao inválida");
--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,4 +1,6 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tarefa {
